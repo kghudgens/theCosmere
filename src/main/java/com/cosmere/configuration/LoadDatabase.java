df0a35403022fb313897d1faf510168998c31dcd
Kevin Hudgens <hudgens1073@gmail.com>
2024-05-27 22:25:56 -0500

    save data to database

--- src/main/java/com/cosmere/configuration/LoadDatabase.java
+++ src/main/java/com/cosmere/configuration/LoadDatabase.java
@@ -4,10 +4,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-import com.cosmere.books.Books;
+import com.cosmere.entities.Books;
 import com.cosmere.repository.BookRepository;
 
+@Configuration
 public class LoadDatabase {
     private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
 
